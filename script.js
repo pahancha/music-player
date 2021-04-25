30e815c84b7773dcacc1aa9498e9fbc7e40f7708
Pahan <pahanchathuranga123@gmail.com>
2021-04-26 00:24:31 +0530

    JavaScript file is added. There are some errors to be corrected

---
+++ script.js
@@ -0,0 +1,77 @@
+const musicContainer = document.querySelector('.music-container')
+const playBtn = document.querySelector('#play')
+const prevBtn = document.querySelector('#prev')
+const nextBtn = document.querySelector('#next')
+const audio = document.querySelector('#audio')
+const progress = document.querySelector('.progress')
+const progressContainer = document.querySelector('.progress-container')
+const title = document.querySelector('#title')
+const cover = document.querySelector('#cover')
+
+//name of the music files to be displayed as the title.
+const songs = ['calming', 'electronic', 'violin']
+
+let songIndex = 2
+
+//Load song into DOM
+loadSong(songs[songIndex])
+
+
+function loadSong(song) {
+    title.innerText = song
+    audio.src = 'music/ ${song}.mp3'
+    cover.src = 'images/ ${song}.jpg'
+}
+
+function playSong() {
+    musicContainer.classList.add('play')
+    playBtn.querySelector('i.fas').classList.remove('fa-play')
+    playBtn.querySelector('i.fas').classList.add('fa-pause')
+
+    audio.play()
+}
+
+function pauseSong() {
+    musicContainer.classList.remove('play')
+    playBtn.querySelector('i.fas').classList.add('fa-play')
+    playBtn.querySelector('i.fas').classList.remove('fa-pause')
+
+    audio.pause()
+}
+
+//Event listers
+playBtn.addEventListener('click', () =>{
+    const isPlaying = musicContainer.classList.contains('play')
+
+    if(isPlaying) {
+        pauseSong()
+    }else {
+        playSong()
+    }
+})
+
+function prevSong() {
+    songIndex--
+
+    if(songIndex < 0){
+        songIndex = song.length - 1
+    }
+
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+function nextSong() {
+    songIndex++
+
+    if(songIndex > songs.length - 1){
+        songIndex = 0
+    }
+
+    loadSong(songs[songIndex])
+    playSong()
+}
+
+prevBtn.addEventListener('click', () =>{
+
+})
